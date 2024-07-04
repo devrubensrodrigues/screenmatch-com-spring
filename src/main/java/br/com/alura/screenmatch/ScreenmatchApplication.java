14e8d62822c540f516c985c98dd28dcef8e2e3ba
Rubens <paulo-rolim10@hotmail.com>
2024-07-04 09:34:18 -0300

    Criando a classe DadosTemporada e melhorias na aplicação

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadosEpsodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -37,5 +41,19 @@ public class ScreenmatchApplication implements CommandLineRunner {
 				DadosEpsodio.class);
 		System.out.println("Formato convertido em DadosEpsodio");
 		System.out.println(dadosEpsodio);
+
+		System.out.println("Formato convertido em DadosTemporada");
+		List<DadosTemporada> list = new ArrayList<>();
+		DadosTemporada dadosTemporada;
+		for (int i = 1; i <= serie1.totalTemporadas(); i++){
+			json = consumoAPI.obterDados(
+					"https://www.omdbapi.com/?t="
+							+ "gilmore+girls&season="
+							+ i
+							+ "&apikey=53fd5cbd");
+			list.add(dadosTemporada = conversor.obterDados(json,
+					DadosTemporada.class));
+		}
+		list.forEach(System.out::println);
 	}
 }
